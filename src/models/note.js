e3a7d4226da1cff15f65b32e29e3c5832c58e7e3
VASYL SPASHCHUK <buyapple0205@gmail.com>
2026-06-02 21:14:05 +0300

    my hw-3

--- src/models/note.js
+++ src/models/note.js
@@ -1,34 +1,25 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
     title: {
-      type: String,
-      required: true,
-      trim: true,
+    type: String,
+    required: true,
+    trim: true,
     },
 
     content: {
-      type: String,
-      default: '',
-      trim: true,
+    type: String,
+    default: '',
+    trim: true,
     },
 
     tag: {
-      type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
-      default: 'Todo',
+        type: String,
+        enum: TAGS,
+        index: true,
+        default: 'Todo',
     },
   },
   {
